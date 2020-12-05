c3a2e422b33104be42be44f922ee7403842a0844
damianlapa <damian.maksymilian.lapa@gmail.com>
2020-12-05 11:14:20 +0100

    keys hide

--- warehousemanager/views.py
+++ warehousemanager/views.py
@@ -1130,7 +1130,20 @@ class GoogleSheetTest(View):
 
         scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
 
-        creds = ServiceAccountCredentials.from_json_keyfile_name('warehousemanager/paker-creds.json', scope)
+        creds_dict ={
+                "type": "service_account",
+                "project_id": os.environ['PROJECT_ID'],
+                "private_key_id": os.environ['PRIVATE_KEY_ID'],
+                "private_key": os.environ['PRIVATE_KEY_GS'],
+                "client_email": os.environ['CLIENT_EMAIL'],
+                "client_id": "104232893114756436399",
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                "client_x509_cert_url": os.environ['CLIENT_CERT_URL'],
+            }
+
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 
         client = gspread.authorize(creds)
 
