b3f0edf5973023b51e14ced890e8831b9b863d5c
damianlapa <damian.maksymilian.lapa@gmail.com>
2021-10-10 11:28:32 +0200

    orders filters

--- warehousemanager/static/js/production-orders.js
+++ warehousemanager/static/js/production-orders.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const allOrders = document.getElementsByClassName('production-order')
     const ordersBtns = document.getElementsByClassName('production-order-btn')
 
+    const ordersFilterInput = document.getElementById('production-orders-filter')
+
     function ordersShowHide(orders, value) {
         for (let i=0; i < orders.length; i++){
             if (orders[i].dataset.orderstatus === value){
@@ -15,9 +17,38 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    function ordersFilter(orders, value) {
+        for (let i=0; i < orders.length; i++){
+            if (orders[i].dataset.orderstatus.toLowerCase().includes(value)){
+            orders[i].style.display = 'block'
+            }
+            else if (orders[i].dataset.idnumber.toLowerCase().includes(value)){
+            orders[i].style.display = 'block'
+            }else if (orders[i].dataset.cardboard.toLowerCase().includes(value)){
+            orders[i].style.display = 'block'
+            }else if (orders[i].dataset.cardboarddimensions.toLowerCase().includes(value)){
+            orders[i].style.display = 'block'
+            }else if (orders[i].dataset.customer.toLowerCase().includes(value)){
+            orders[i].style.display = 'block'
+            }else if (orders[i].dataset.quantity.toLowerCase().includes(value)){
+            orders[i].style.display = 'block'
+            }else if (orders[i].dataset.notes.toLowerCase().includes(value)){
+            orders[i].style.display = 'block'
+            }else if (orders[i].dataset.dimensions.toLowerCase().includes(value)){
+            orders[i].style.display = 'block'
+            }else{
+            orders[i].style.display = 'none'
+            }
+        }
+    }
+
     for (let i=0; i < ordersBtns.length; i++){
         ordersBtns[i].addEventListener('click', function (){
             ordersShowHide(allOrders, this.innerText)
         })
     }
+
+    ordersFilterInput.addEventListener('keyup', function(){
+        ordersFilter(allOrders, this.value.toLowerCase())
+    })
 })
