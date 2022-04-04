d7d7f7b353ad8fea49844d3241e49f6cdbdd20ac
damianlapa <damian.maksymilian.lapa@gmail.com>
2022-04-04 12:25:38 +0200

    timetable work end

--- warehousemanager/views.py
+++ warehousemanager/views.py
@@ -2470,7 +2470,17 @@ class MonthlyCardPresence(View):
             start_hour = f'{day_start.hour}' if day_start.hour >= 10 else f'0{day_start.hour}'
             start_minute = f'{day_start.minute}' if day_start.minute >= 10 else f'0{day_start.minute}'
             day_start_str = f'{start_hour}:{start_minute}'
-            day_info.append(day_start_str) if day.date() >= worker.job_start else day_info.append('')
+            #day_info.append(day_start_str) if day.date() >= worker.job_start else day_info.append('')
+            if day.date() >= worker.job_start:
+                if worker.job_end:
+                    if day.date() <= worker.job_end:
+                        day_info.append(day_start_str)
+                    else:
+                        day_info.append('')
+                else:
+                    day_info.append('')
+            else:
+                day_info.append('')
 
             # work end hour
             day_end = day + datetime.timedelta(hours=15)
@@ -2489,22 +2499,41 @@ class MonthlyCardPresence(View):
             end_hour = f'{day_end.hour}' if day_end.hour >= 10 else f'0{day_end.hour}'
             end_minute = f'{day_end.minute}' if day_end.minute >= 10 else f'0{day_end.minute}'
             day_end_str = f'{end_hour}:{end_minute}'
-            day_info.append(day_end_str) if day.date() >= worker.job_start else day_info.append('')
+            # day_info.append(day_end_str) if day.date() >= worker.job_start else day_info.append('')
+            if day.date() >= worker.job_start:
+                if worker.job_end:
+                    if day.date() <= worker.job_end:
+                        day_info.append(day_start_str)
+                    else:
+                        day_info.append('')
+                else:
+                    day_info.append('')
+            else:
+                day_info.append('')
 
             if day.date() >= worker.job_start:
 
-                # hours at work
-                hours_at_work = f'{day_end - day_start}'
-                divided_time = hours_at_work.split(':')
-                hours_at_work = round(float(divided_time[0]) + float(divided_time[1]) / 60, 2)
-                day_info.append(hours_at_work)
-                if not holiday:
-                    if day_info[1] not in ('So', 'Nd'):
-                        if not absences:
-                            summary[4] += hours_at_work
-                        else:
-                            if absence_type == 'SP' or absence_type == 'D':
+                job_end = False
+
+                if worker.job_end:
+                    if day.date() > worker.job_end:
+                        job_end = True
+
+                if not job_end:
+                    # hours at work
+                    hours_at_work = f'{day_end - day_start}'
+                    divided_time = hours_at_work.split(':')
+                    hours_at_work = round(float(divided_time[0]) + float(divided_time[1]) / 60, 2)
+                    day_info.append(hours_at_work)
+                    if not holiday:
+                        if day_info[1] not in ('So', 'Nd'):
+                            if not absences:
                                 summary[4] += hours_at_work
+                            else:
+                                if absence_type == 'SP' or absence_type == 'D':
+                                    summary[4] += hours_at_work
+                else:
+                    day_info.append('')
 
             else:
                 day_info.append('')
