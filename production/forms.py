9902dcbb03ae71cda6e1af326b5dab12cd8e45d5
damianlapa <damian.maksymilian.lapa@gmail.com>
2023-09-18 10:21:59 +0200

    select size

--- production/forms.py
+++ production/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, TextInput, Select, DateTimeInput
+from django.forms import ModelForm, TextInput, Select, DateTimeInput, SelectMultiple
 from production.models import *
 from warehousemanager.models import Person
 
@@ -9,22 +9,27 @@ class ProductionOrderForm(ModelForm):
         # fields = ('id_number', 'cardboard', 'cardboard_dimensions', 'customer', 'dimensions', 'quantity', 'status', 'notes')
         fields = '__all__'
 
+
 class ProductionUnitForm(ModelForm):
-    class Meta:
-        model = ProductionUnit
-        fields = '__all__'
-        widgets = {
-            'start': DateTimeInput(attrs={'type': 'datetime'}),
-            'end': DateTimeInput(attrs={'type': 'datetime'}),
-        }
+    workers_num = 0
 
     def __init__(self, day=None, *args, **kwargs):
         super(ProductionUnitForm, self).__init__(*args, **kwargs)
         all_workers = Person.objects.all()
         if day:
-            print('DATE', day)
             c_workers = Person.objects.filter(job_start__lte=day, job_end__isnull=True)
 
             all_workers = c_workers
 
         self.fields['persons'].queryset = all_workers
+        workers_num = len(all_workers)
+
+    class Meta:
+        size = len(Person.objects.filter(job_start__lte=datetime.datetime.today(), job_end__isnull=True))
+        model = ProductionUnit
+        fields = '__all__'
+        widgets = {
+            'start': DateTimeInput(attrs={'type': 'datetime'}),
+            'end': DateTimeInput(attrs={'type': 'datetime'}),
+            'persons': SelectMultiple(attrs={'size': f'{size}'})
+        }
