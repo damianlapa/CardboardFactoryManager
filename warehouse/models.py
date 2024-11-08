1b7032469d07bcf18e842c56a87f6c211e45e6c2
damianlapa <58368009+damianlapa@users.noreply.github.com>
2024-11-08 11:43:07 +0100

    update models

--- warehouse/models.py
+++ warehouse/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from warehousemanager.models import Buyer
 
 
 UNITS = (
@@ -9,12 +10,79 @@ UNITS = (
 )
 
 
+class Palette(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
 class Provider(models.Model):
     name = models.CharField(max_length=64)
     shortcut = models.CharField(max_length=24, null=True, blank=True)
 
     def __str__(self):
         return f'{self.name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Buyer, on_delete=models.PROTECT)
+    provider = models.ForeignKey(Provider, on_delete=models.PROTECT)
+    order_id = models.CharField(max_length=32)
+    customer_date = models.DateField()
+    order_date = models.DateField(null=True, blank=True)
+    delivery_date = models.DateField(null=True, blank=True)
+    production_date = models.DateField(null=True, blank=True)
+    dimensions = models.CharField(max_length=32)
+    name = models.CharField(max_length=32)
+    weight = models.PositiveIntegerField(default=0)
+    order_quantity = models.PositiveIntegerField()
+    delivered_quantity = models.PositiveIntegerField(default=0)
+    price = models.PositiveIntegerField(default=0)
+    product = models.ForeignKey(Product, null=True, blank=True, on_delete=models.PROTECT)
+    delivered = models.BooleanField(default=False)
+    finished = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.provider} {self.order_id} {self.name}'
+
+
+class Delivery(models.Model):
+    provider = models.ForeignKey(Provider, on_delete=models.PROTECT)
+    date = models.DateField()
+    car_number = models.CharField(max_length=16, null=True, blank=True)
+    telephone = models.CharField(max_length=16, null=True, blank=True)
+    description = models.CharField(max_length=256, null=True, blank=True)
+    palettes = models.ManyToManyField(Palette, through='DeliveryPalette')
+
+    def __str__(self):
+        return f'{self.provider}({self.car_number}) {self.date}'
+
+
+class DeliveryItem(models.Model):
+    delivery = models.ForeignKey(Delivery, on_delete=models.PROTECT)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.delivery} :: {self.order}'
+
+
+class DeliveryPalette(models.Model):
+    delivery = models.ForeignKey(Delivery, on_delete=models.PROTECT)
+    palette = models.ForeignKey(Palette, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.delivery} :: {self.palette} :: {self.quantity}'
 
 
 class StockType(models.Model):
