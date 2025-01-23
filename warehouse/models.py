3ed755aa7da11d3f70a6a844e14abea1e67dc3fa
damianlapa <58368009+damianlapa@users.noreply.github.com>
2025-01-23 12:23:21 +0100

    warehouse description

--- warehouse/models.py
+++ warehouse/models.py
@@ -201,6 +201,7 @@ class Stock(models.Model):
 
 class Warehouse(models.Model):
     name = models.CharField(max_length=64)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.name}'
