f224a0b569dbdb1df6673dcc812c47dd295042b3
damianlapa <58368009+damianlapa@users.noreply.github.com>
2025-01-23 12:06:07 +0100

    test_migration

--- paker/urls.py
+++ paker/urls.py
@@ -16,6 +16,12 @@ urlpatterns = [
     path('open-order/<int:order_item_id>/', OpenFile.as_view(), name='open-order'),
     path('format-converter/', FormatConverter.as_view(), name='format-converter'),
     path('add-note/', NoteAdd.as_view(), name='add-note'),
+
+
+
+
+
+
     path('notes/', AllNotes.as_view(), name='notes'),
     path('note-details/<int:note_id>/', NoteDetailsView.as_view(), name='note-details'),
     path('note-delete/<int:note_id>/', NoteDeleteView.as_view(), name='note-delete'),
