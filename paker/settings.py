4dc473b4349e10bc30ba4ced6f0cb9dcfd9fb3b1
damianlapa <58368009+damianlapa@users.noreply.github.com>
2025-07-02 13:03:03 +0200

    remove ddt

--- paker/settings.py
+++ paker/settings.py
@@ -49,8 +49,6 @@ INSTALLED_APPS = [
     'finances',
     'customers',
     'warehouse',
-    'debug_toolbar',
-
 ]
 
 MIDDLEWARE = [
@@ -86,10 +84,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'paker.wsgi.application'
-
-INTERNAL_IPS = [
-    '127.0.0.1', '10.0.2.2'
-]
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
--- paker/urls.py
+++ paker/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from warehousemanager.views import *
 from django.conf.urls.static import static
-import debug_toolbar
 
 
 urlpatterns = [
@@ -222,4 +221,3 @@ urlpatterns += [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
