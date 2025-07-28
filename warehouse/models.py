296b931283f7295a11d3fadc434dfa3b95c9f762
damianlapa <58368009+damianlapa@users.noreply.github.com>
2025-07-28 13:58:43 +0200

    Update models.py

--- warehouse/models.py
+++ warehouse/models.py
@@ -547,25 +547,25 @@ class ProductSell3(models.Model):
         return None
 
 
-class ProductComplex(models.Model):
-    name = models.CharField(max_length=64, unique=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
-    dimensions = models.CharField(max_length=32, null=True, blank=True)
-    flute = models.CharField(max_length=8, null=True, blank=True)
-    gsm = models.PositiveIntegerField(default=0)
-    parts = models.ManyToManyField(Product, through='ProductComplexParts', blank=True)
-
-    def __str__(self):
-        return f'{self.name}'
-
-    class Meta:
-        ordering = ['name']
-
-
-class ProductComplexParts(models.Model):
-    product = models.ForeignKey(ProductComplex, on_delete=models.PROTECT)
-    part = models.ForeignKey(Product, on_delete=models.PROTECT)
-    quantity = models.PositiveIntegerField(default=0)
-
-    def __str__(self):
-        return f'{self.product} :: {self.part} :: {self.quantity}'
+# class ProductComplex(models.Model):
+#     name = models.CharField(max_length=64, unique=True)
+#     price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+#     dimensions = models.CharField(max_length=32, null=True, blank=True)
+#     flute = models.CharField(max_length=8, null=True, blank=True)
+#     gsm = models.PositiveIntegerField(default=0)
+#     parts = models.ManyToManyField(Product, through='ProductComplexParts', blank=True)
+#
+#     def __str__(self):
+#         return f'{self.name}'
+#
+#     class Meta:
+#         ordering = ['name']
+#
+#
+# class ProductComplexParts(models.Model):
+#     product = models.ForeignKey(ProductComplex, on_delete=models.PROTECT)
+#     part = models.ForeignKey(Product, on_delete=models.PROTECT)
+#     quantity = models.PositiveIntegerField(default=0)
+#
+#     def __str__(self):
+#         return f'{self.product} :: {self.part} :: {self.quantity}'
