15710df8e9cf914dc424dfb4eb5020df04522117
damianlapa <58368009+damianlapa@users.noreply.github.com>
2025-08-14 08:41:16 +0200

    migrations

--- warehouse/migrations/0018_deliveryspecial_alter_productcomplexassembly_options_and_more.py
+++ warehouse/migrations/0018_deliveryspecial_alter_productcomplexassembly_options_and_more.py
@@ -42,7 +42,7 @@ class Migration(migrations.Migration):
             name='DeliverySpecialItem',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField()),
+                ('name', models.CharField(max_length=64)),
                 ('quantity', models.PositiveIntegerField(default=0)),
                 ('price', models.DecimalField(decimal_places=2, default=0.0, max_digits=5)),
                 ('processed', models.BooleanField(default=False)),
