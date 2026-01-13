8e0702b2fa3a0cf73b92b9a7d62dd2120d5fbb28
damianlapa <58368009+damianlapa@users.noreply.github.com>
2026-01-13 15:48:16 +0100

    Update views.py

--- warehouse/views.py
+++ warehouse/views.py
@@ -1080,8 +1080,10 @@ class LoadDeliveryToGSFile(LoginRequiredMixin, View):
             item.save()
             order_id = item.order.order_id
             number, year = map(int, order_id.split('/'))
-            if len(year) == 2:
-                year = '20' + year
+            if len(str(year)) == 2:
+                year = '20' + str(year)
+            else:
+                year = str(year)
             if year in data.keys():
                 data[year][0].append(number)
                 data[year][1].append(item.quantity)
